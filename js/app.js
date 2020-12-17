816b4ec8315180db8bc2accfa971ed29fd1bb402
bcn-alumno <bcn-alumno@nm_madrid.local>
2020-12-17 12:48:44 +0100

    Exercise done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 // Tu código aquí
+let button = document.querySelector('#submitBtn')
+
+button.onclick = getValue
 
 const form = document.querySelector('#message-form')
 
@@ -9,3 +12,19 @@ form.addEventListener('submit', function(e){
     console.log("Hemos hecho clic en el botón de submit!")
 
 })
+
+function getValue(){
+    var message = document.querySelector("#message").value;
+
+    if (message === "") {
+        message = "Enter a value";
+        document.querySelector(".feedback").style.display = "block";
+        document.querySelector(".feedback").innerHTML = message;
+        document.querySelector(".feedback").style.transitionDuration = "5s";
+        window.setTimeout(function () {
+            document.querySelector(".feedback").style.display = "none";
+        }, 2000)
+    }else{ 
+        document.querySelector(".message-content").innerHTML = message;
+    }
+}
